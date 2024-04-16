48050d93b8982e3666f7a216e529efc35fc4b133
tsicha-aa <167217513+tsicha-aa@users.noreply.github.com>
2024-04-16 16:31:57 +0200

    Update Initialize-OSDCloudCustom000.ps1

--- Initialize-OSDCloudCustom000.ps1
+++ Initialize-OSDCloudCustom000.ps1
@@ -1,9 +1,12 @@
-Install-Module OSD -Force
 Import-Module OSD -Force
 
 $OSDModuleResource.OSDCloud.Values.Language="de-de","en-us"
 $OSDModuleResource.OSDCloud.Default.Language="de-de"
-
+$OSDModuleResource.OSDCloud.Values.Edition="Enterprise","Pro"
+$OSDModuleResource.StartOSDCloudGUI.updateFirmware=$true
+$OSDModuleResource.StartOSDCloudGUI.WindowsUpdate=$true
+$OSDModuleResource.StartOSDCloudGUI.updateFirmware=$true
+$OSDModuleResource.StartOSDCloudGUI.WindowsUpdateDrivers=$true
 Start-OSDCloudGUI -Brand "Anita Services GmbH"
 
-wpeutil reboot
+#wpeutil reboot
