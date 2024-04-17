4ae208172d1fc6134a07e3f63985d5fc0c5c1903
tsicha-aa <167217513+tsicha-aa@users.noreply.github.com>
2024-04-17 12:19:04 +0200

    Update Initialize-OSDCloudCustom000.ps1

--- Initialize-OSDCloudCustom000.ps1
+++ Initialize-OSDCloudCustom000.ps1
@@ -26,7 +26,41 @@ if ((Get-MyComputerManufacturer -Brief) -eq 'Dell') {
 			&"$env:temp\$DellCCTK\cctk.exe" -i "$env:temp\$DellBiosSettingsFile"
 		}
 	}
+}Import-Module OSD -Force
+
+$GitRawPath="https://raw.githubusercontent.com/tsicha-aa/OSD-PUB/main"
+$DellCCTK="DellCCTK4.1"
+$DellBiosSettingsFile="Bios-Settings.cctk"
+
+$OSDModuleResource.OSDCloud.Values.Language="en-us","de-de","cs-cz","nl-nl","fr-fr","it-it","pl-pl","pt-pt","es-es","th-th"
+$OSDModuleResource.OSDCloud.Default.Language="de-de"
+$OSDModuleResource.OSDCloud.Values.Edition="Enterprise","Pro"
+$OSDModuleResource.StartOSDCloudGUI.updateFirmware=$true
+$OSDModuleResource.StartOSDCloudGUI.WindowsUpdate=$true
+$OSDModuleResource.StartOSDCloudGUI.updateFirmware=$true
+$OSDModuleResource.StartOSDCloudGUI.WindowsUpdateDrivers=$true
+$OSDModuleResource.StartOSDCloudGUI.ClearDiskConfirm=$false
+
+if ((Get-MyComputerManufacturer -Brief) -eq 'Dell') {
+	Add-Type -AssemblyName PresentationFramework
+	$msgBoxInput =  [System.Windows.MessageBox]::Show('Update Dell Bios Settings?','BIOS Settings','YesNoCancel','Question')
+	switch  ($msgBoxInput) {
+		'Yes' {
+			Write-Host -ForegroundColor Yellow "Download Dell CCTK and Bios-Settings File"
+			Save-WebFile -SourceUrl $GitRawPath/BIOS/$DellCCTK.zip -DestinationDirectory "$env:temp"
+			Save-WebFile -SourceUrl $GitRawPath/BIOS/Bios-Settings.cctk -DestinationDirectory "$env:temp"
+			Expand-Archive "$env:temp\$DellCCTK.zip" -DestinationPath "$env:temp\$DellCCTK" -Force
+			Write-Host -ForegroundColor Yellow "Changing Dell-Bios Settings:"
+			&"$env:temp\$DellCCTK\cctk.exe" -i "$env:temp\$DellBiosSettingsFile"
+		}
+	}
 }
+
+Start-OSDCloudGUI -Brand "Anita Services GmbH"
+
+#wpeutil reboot
+
+
 
 Start-OSDCloudGUI -Brand "Anita Services GmbH"
 
